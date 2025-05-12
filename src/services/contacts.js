4fe85c59d107691b1382e6cbb9bdb51fb787a14b
Turetska Inna <innaturetska1987@gmail.com>
2025-05-12 15:08:10 +0300

    update patch

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,32 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const patchContact = async (contactId, payload, options = {}) => {
+  const updateContact = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+  if (!updateContact || !updateContact.value) {
+    return null;
+  }
+  return {
+    contact: updateContact.value,
+    isNew: false,
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findOneAndDelete({ _id: contactId });
+  return contact;
+};
