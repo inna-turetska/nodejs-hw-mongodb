0b1c0747e073dc5ccab682ad3eaec7dd7b737e15
Turetska Inna <innaturetska1987@gmail.com>
2025-05-16 12:02:49 +0300

    update validId

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,7 +2,7 @@ import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 
 export const isValidId = (req, res, next) => {
-  const contactId = req.params;
+  const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
     throw createHttpError(400, 'Bad Request');
   }
